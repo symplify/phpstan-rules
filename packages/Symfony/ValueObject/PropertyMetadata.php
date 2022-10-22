7343361eeff34ccfdc726964bf798cddeacf81fc
GitHub Action <action@github.com>
2022-10-22 13:46:14 +0000

    Updated PHPStan Rules to commit 3786b8d526e89f5c1061f2392a498986e0298fbb
    https://github.com/symplify/symplify/commit/3786b8d526e89f5c1061f2392a498986e0298fbb [PHPStanRules] Add RequireCascadeValidateRule (#4443)

---
+++ packages/Symfony/ValueObject/PropertyMetadata.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Symfony\ValueObject;
+
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionProperty;
+use PHPStan\Reflection\Php\PhpPropertyReflection;
+use PHPStan\Type\Type;
+use Symplify\PHPStanRules\Exception\ShouldNotHappenException;
+
+final class PropertyMetadata
+{
+    /**
+     * @var \PHPStan\Reflection\Php\PhpPropertyReflection
+     */
+    private $phpPropertyReflection;
+    /**
+     * @var \PHPStan\BetterReflection\Reflection\Adapter\ReflectionProperty
+     */
+    private $nativeReflectionProperty;
+    /**
+     * @var int
+     */
+    private $propertyLine;
+    public function __construct(PhpPropertyReflection $phpPropertyReflection, ReflectionProperty $nativeReflectionProperty, int $propertyLine)
+    {
+        $this->phpPropertyReflection = $phpPropertyReflection;
+        $this->nativeReflectionProperty = $nativeReflectionProperty;
+        $this->propertyLine = $propertyLine;
+    }
+
+    public function getPropertyType(): Type
+    {
+        return $this->phpPropertyReflection->getReadableType();
+    }
+
+    public function getDocComment(): string
+    {
+        return (string) $this->phpPropertyReflection->getDocComment();
+    }
+
+    public function getFileName(): string
+    {
+        $reflectionClass = $this->nativeReflectionProperty->getDeclaringClass();
+
+        $fileName = $reflectionClass->getFileName();
+        if ($fileName === false) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $fileName;
+    }
+
+    public function getPropertyName(): string
+    {
+        return $this->nativeReflectionProperty->getName();
+    }
+
+    public function getPropertyLine(): int
+    {
+        return $this->propertyLine;
+    }
+}
