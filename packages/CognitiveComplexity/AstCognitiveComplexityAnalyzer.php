d38219e2e057b1e8c74930618dd706b291d72e16
GitHub Action <action@github.com>
2022-12-23 13:14:36 +0000

    Updated PHPStan Rules to commit 34ca4b3156cc939bd85bbbf3ca80df94d22435e3
    https://github.com/symplify/symplify/commit/34ca4b3156cc939bd85bbbf3ca80df94d22435e3 [PHPStanRules] Deprecated cognitive rules, use new package instead (#4515)

--- packages/CognitiveComplexity/AstCognitiveComplexityAnalyzer.php
+++ packages/CognitiveComplexity/AstCognitiveComplexityAnalyzer.php
@@ -10,11 +10,14 @@ use PhpParser\Node\Stmt\Function_;
 use Symplify\PHPStanRules\CognitiveComplexity\DataCollector\CognitiveComplexityDataCollector;
 use Symplify\PHPStanRules\CognitiveComplexity\NodeTraverser\ComplexityNodeTraverserFactory;
 use Symplify\PHPStanRules\CognitiveComplexity\NodeVisitor\NestingNodeVisitor;
+use Symplify\PHPStanRules\Exception\DeprecatedException;
 
 /**
  * @see \Symplify\PHPStanRules\Tests\CognitiveComplexity\AstCognitiveComplexityAnalyzer\AstCognitiveComplexityAnalyzerTest
  *
  * implements the concept described in https://www.sonarsource.com/resources/white-papers/cognitive-complexity/
+ *
+ * @deprecated
  */
 final class AstCognitiveComplexityAnalyzer
 {
@@ -39,12 +42,12 @@ final class AstCognitiveComplexityAnalyzer
 
     public function analyzeClassLike(Class_ $class): int
     {
-        $totalCognitiveComplexity = 0;
-        foreach ($class->getMethods() as $classMethod) {
-            $totalCognitiveComplexity += $this->analyzeFunctionLike($classMethod);
-        }
-
-        return $totalCognitiveComplexity;
+        $deprecatedMessage = sprintf(
+            'The "%s" service was deprecated and moved to "%s" package that has much simpler configuration. Use it instead.',
+            self::class,
+            'https://github.com/TomasVotruba/cognitive-complexity'
+        );
+        throw new DeprecatedException($deprecatedMessage);
     }
 
     /**
@@ -53,12 +56,11 @@ final class AstCognitiveComplexityAnalyzer
      */
     public function analyzeFunctionLike($functionLike): int
     {
-        $this->cognitiveComplexityDataCollector->reset();
-        $this->nestingNodeVisitor->reset();
-
-        $nodeTraverser = $this->complexityNodeTraverserFactory->create();
-        $nodeTraverser->traverse([$functionLike]);
-
-        return $this->cognitiveComplexityDataCollector->getCognitiveComplexity();
+        $deprecatedMessage = sprintf(
+            'The "%s" service was deprecated and moved to "%s" package that has much simpler configuration. Use it instead.',
+            self::class,
+            'https://github.com/TomasVotruba/cognitive-complexity'
+        );
+        throw new DeprecatedException($deprecatedMessage);
     }
 }
