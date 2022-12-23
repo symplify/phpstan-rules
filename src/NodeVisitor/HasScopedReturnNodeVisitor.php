2b9ca1ac53ff67ac6a2a6abb758252d5961197e3
GitHub Action <action@github.com>
2022-12-23 01:22:55 +0000

    Updated PHPStan Rules to commit a163cf39ce15ac3cd8f414abef527f2ed99f7c07
    https://github.com/symplify/symplify/commit/a163cf39ce15ac3cd8f414abef527f2ed99f7c07 [PHPStanRules] Skip nested closure in NoReturnSetterMethodRule

---
+++ src/NodeVisitor/HasScopedReturnNodeVisitor.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\NodeVisitor;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+
+final class HasScopedReturnNodeVisitor extends NodeVisitorAbstract
+{
+    /**
+     * @var bool
+     */
+    private $hasReturn = false;
+    public function __construct(bool $hasReturn = false)
+    {
+        $this->hasReturn = $hasReturn;
+    }
+
+    /**
+     * @return int|\PhpParser\Node|null
+     */
+    public function enterNode(Node $node)
+    {
+        if ($node instanceof Node\Expr\Closure) {
+            return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
+        }
+
+        if (! $node instanceof Return_) {
+            return null;
+        }
+
+        if (! $node->expr instanceof Expr) {
+            return null;
+        }
+
+        $this->hasReturn = true;
+        return $node;
+    }
+
+    public function hasReturn(): bool
+    {
+        return $this->hasReturn;
+    }
+}
