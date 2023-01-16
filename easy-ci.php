a3e9b4bef15f5b097f9054d4ddb8795ed3a96767
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-17 04:48:34 +0700

    add easy-ci type to skip

--- easy-ci.php
+++ easy-ci.php
@@ -1,14 +1,13 @@
-?php
+<?php
 
 declare(strict_types=1);
 
-use Symfony\Component\Console\Application;
 use Symplify\EasyCI\Config\EasyCIConfig;
-use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
 
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
-        AbstractSymplifyKernel::class,
-        Application::class,
+        \Symplify\PHPStanRules\Exception\DeprecatedException::class,
+        \Symplify\PHPStanRules\NodeAnalyzer\AttributeFinder::class,
+        \Symplify\PHPStanRules\NodeVisitor\AssignedToPropertyNodeVisitor::class,
     ]);
 };
