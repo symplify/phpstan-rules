3137da56cfbe4925aa88d7c1d20750e77b9d2b47
Tomas Votruba <tomas.vot@gmail.com>
2023-05-05 13:57:51 +0200

    Skip OutputInterface in sprintf (#44)

--- rector.php
+++ rector.php
@@ -37,6 +37,6 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->ruleWithConfiguration(\Rector\Php55\Rector\String_\StringClassNameToClassConstantRector::class, [
-        'Symfony\Component\Console\Command\Command',
+        'Symfony\Component\Console\*',
     ]);
 };
