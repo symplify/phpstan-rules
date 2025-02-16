2cbac09af309eb9504a7bb6a4bd9ca1e67bdf966
Tomas Votruba <tomas.vot@gmail.com>
2025-02-16 22:51:47 +0100

    [symfony] Add NoRoutingPrefixRule (#172)

---
+++ tests/Rules/Symfony/NoRoutingPrefixRule/Fixture/skip_no_route_prefix.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return static function (RoutingConfigurator $routingConfigurator): void {
+    $routingConfigurator->import(__DIR__ . '/some-path');
+};
