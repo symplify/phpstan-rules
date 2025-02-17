1ea315d5f5ca1e37708f7e2d85b752f47bd067c7
Tomas Votruba <tomas.vot@gmail.com>
2025-02-17 10:58:55 +0100

    [symfony] Add NoFindTaggedServiceIdsCallRule

---
+++ src/Rules/Symfony/NoFindTaggedServiceIdsCallRule.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules\Symfony;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\IdentifierRuleError;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use Symplify\PHPStanRules\Enum\SymfonyRuleIdentifier;
+
+/**
+ * @implements Rule<MethodCall>
+ */
+final class NoFindTaggedServiceIdsCallRule implements Rule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Instead of "$this->findTaggedServiceIds()" use more reliable registerForAutoconfiguration() and tagged iterator attribute. Those work outside any configuration and avoid missed tag errors';
+
+    public function getNodeType(): string
+    {
+        return MethodCall::class;
+    }
+
+    /**
+     * @param MethodCall $node
+     * @return IdentifierRuleError[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (! $node->name instanceof Identifier) {
+            return [];
+        }
+
+        if ($node->name->toString() !== 'findTaggedServiceIds') {
+            return [];
+        }
+
+        $identifierRuleError = RuleErrorBuilder::message(self::ERROR_MESSAGE)
+            ->identifier(SymfonyRuleIdentifier::class)
+            ->build();
+
+        return [
+            $identifierRuleError,
+        ];
+    }
+}
