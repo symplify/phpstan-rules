720a146e75272e1fa859d50326f627bb04a47e2a
Tomas Votruba <tomas.vot@gmail.com>
2025-03-12 11:47:52 +0100

    [rule] Param name to type convention (#187)

---
+++ src/Rules/Convention/ParamNameToTypeConventionRule.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Rules\Convention;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Param;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleError;
+use PHPStan\Rules\RuleErrorBuilder;
+use Symplify\PHPStanRules\Enum\RuleIdentifier;
+use Webmozart\Assert\Assert;
+
+/**
+ * @implements Rule<Param>
+ * @see \Symplify\PHPStanRules\Tests\Rules\Convention\ParamNameToTypeConventionRule\ParamNameToTypeConventionRuleTest
+ */
+final class ParamNameToTypeConventionRule implements Rule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Parameter name "$%s" should probably have "%s" type';
+
+    /**
+     * @param array<string, string> $paramNamesToTypes
+     */
+    public function __construct(
+        private array $paramNamesToTypes
+    ) {
+        Assert::notEmpty($paramNamesToTypes);
+
+        Assert::allString(array_keys($paramNamesToTypes));
+        Assert::allString($paramNamesToTypes);
+    }
+
+    public function getNodeType(): string
+    {
+        return Param::class;
+    }
+
+    /**
+     * @param Param $node
+     * @return RuleError[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        // param type is known, let's skip it
+        if ($node->type instanceof Node) {
+            return [];
+        }
+
+        // unable to fill the type
+        if ($node->variadic) {
+            return [];
+        }
+
+        if (! $node->var instanceof Variable) {
+            return [];
+        }
+
+        if (! is_string($node->var->name)) {
+            return [];
+        }
+
+        $variableName = $node->var->name;
+
+        $expectedType = $this->paramNamesToTypes[$variableName] ?? null;
+        if ($expectedType === null) {
+            return [];
+        }
+
+        $errorMessage = sprintf(self::ERROR_MESSAGE, $variableName, $expectedType);
+
+        $identifierRuleError = RuleErrorBuilder::message($errorMessage)
+            ->identifier(RuleIdentifier::CONVENTION_PARAM_NAME_TO_TYPE)
+            ->build();
+
+        return [$identifierRuleError];
+    }
+}
