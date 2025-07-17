be64e9c5a4f0ae6877dd0e4e2005b6106a35d170
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-07-18 02:12:16 +0700

    fix install

--- tests/autoload.php
+++ tests/autoload.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../vendor/rector/rector/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/rector/rector-src/vendor/autoload.php';
