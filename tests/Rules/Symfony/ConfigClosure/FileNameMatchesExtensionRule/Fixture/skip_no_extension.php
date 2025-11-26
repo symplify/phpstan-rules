0abcb2859bddc2afb4d9002276644750d3cbf6f3
Tomas Votruba <tomas.vot@gmail.com>
2025-11-26 13:03:45 +0000

    [config] Add FileNameMatchesExtensionRule (#246)

---
+++ tests/Rules/Symfony/ConfigClosure/FileNameMatchesExtensionRule/Fixture/skip_no_extension.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Symplify\PHPStanRules\Tests\Rules\Symfony\ConfigClosure\NoDuplicateArgAutowireByTypeRule\Fixture;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\PHPStanRules\Tests\Rules\Symfony\ConfigClosure\NoDuplicateArgAutowireByTypeRule\Source\AnotherType;
+use Symplify\PHPStanRules\Tests\Rules\Symfony\ConfigClosure\NoDuplicateArgAutowireByTypeRule\Source\SomeServiceWithConstructor;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+return function (ContainerConfigurator $container) {
+    $services = $container->services();;
+};
