3d9c46502852d89688242afef2621d005d9bccc2
GitHub Action <action@github.com>
2025-12-02 13:11:47 +0000

    release PHP 7.4 downgraded

--- tests/Rules/Doctrine/NoGetRepositoryOnServiceRepositoryEntityRule/Source/Repository/RepositoryWithInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Symplify\PHPStanRules\Tests\Rules\Doctrine\NoGetRepositoryOnServiceRepositoryEntityRule\Source\Repository;
-
-use Doctrine\Bundle\MongoDBBundle\Repository\ServiceDocumentRepositoryInterface;
-
-final class RepositoryWithInterface implements ServiceDocumentRepositoryInterface
-{
-}
