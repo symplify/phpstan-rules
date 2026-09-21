010d2ba98638618d985644c57a590814f5b18d1e
Tomas Votruba <tomas.vot@gmail.com>
2026-09-21 13:23:08 +0200

    keep string

--- rector.php
+++ rector.php
@@ -20,6 +20,7 @@ return RectorConfig::configure()
             __DIR__ . '/src/Testing/PHPUnitTestAnalyser.php',
             __DIR__ . '/src/Rules/NoEntityOutsideEntityNamespaceRule.php',
             __DIR__ . '/src/Rules/Symfony/CommandMustHaveAsCommandAttributeRule.php',
+            __DIR__ . '/src/Rules/Doctrine/NoStringTargetEntityRule.php',
             __DIR__ . '/src/Rules/Symfony/ConstraintMustHaveAttributeRule.php',
             __DIR__ . '/tests/Naming/ClassToSuffixResolverTest.php',
             __DIR__ . '/src/Doctrine/DoctrineEntityDocumentAnalyser.php',
