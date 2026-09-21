7b8d17ce28eee94d4cee890e91e5149070efe19e
Tomas Votruba <tomas.vot@gmail.com>
2026-09-21 10:00:02 +0200

    smisc

--- rector.php
+++ rector.php
@@ -18,6 +18,8 @@ return RectorConfig::configure()
             __DIR__ . '/src/Enum',
             __DIR__ . '/src/Testing/PHPUnitTestAnalyser.php',
             __DIR__ . '/src/Rules/NoEntityOutsideEntityNamespaceRule.php',
+            __DIR__ . '/src/Rules/Symfony/CommandMustHaveAsCommandAttributeRule.php',
+            __DIR__ . '/src/Rules/Symfony/ConstraintMustHaveAttributeRule.php',
             __DIR__ . '/tests/Naming/ClassToSuffixResolverTest.php',
             __DIR__ . '/src/Doctrine/DoctrineEntityDocumentAnalyser.php',
         ],
