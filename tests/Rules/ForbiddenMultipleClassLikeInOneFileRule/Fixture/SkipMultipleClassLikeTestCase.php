91975b0918bdecf6e3aaf7c9469b3fd3e00e700d
Tomas Votruba <tomas.vot@gmail.com>
2026-09-25 11:57:55 +0200

    Allow multiple class-likes in test case file in ForbiddenMultipleClassLikeInOneFileRule (#322)

---
+++ tests/Rules/ForbiddenMultipleClassLikeInOneFileRule/Fixture/SkipMultipleClassLikeTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PHPStanRules\Tests\Rules\ForbiddenMultipleClassLikeInOneFileRule\Fixture;
+
+final class SkipMultipleClassLikeTestCase
+{
+}
+
+final class SomeHelperStub
+{
+}
